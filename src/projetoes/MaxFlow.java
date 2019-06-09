d0f38fb6d0487fd9cee72d3dfa6ceec36a1e99a0
casa <casa@casa-To-be-filled-by-O-E-M>
2019-06-09 18:10:10 -0300

    carregar

---
+++ src/projetoes/MaxFlow.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetoes;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author gabri
+ */
+public class MaxFlow
+{
+    public static final int V = 6; // Número de vértices do grafo
+    int rGrafo[][] = new int[V][V];
+    //cria um grafo residua e preenche ele com o grafo residual
+    // retorna verdadeiro se existe um caminho da fonte até o terminal
+    // preenche pai[] que armazena o caminho
+    public boolean BFS(int rGrafo[][], int s, int t, int pai[]) 
+    { 
+        //cria um vertice de visitados e marca todos os vertices como não visitados
+        boolean visitado[] = new boolean[V]; 
+        for(int i=0; i<V; ++i)
+        {
+
+            visitado[i]=false; 
+
+        }
+         //cria uma fila, enfileira o vertice fonte e marca como visitado
+        LinkedList<Integer> fila = new LinkedList<>(); 
+        fila.add(s); 
+        // BFS  
+        while (fila.size()!=0) 
+        { 
+            int u = fila.poll(); 
+            for (int v=0; v<V; v++) 
+            { 
+                if (visitado[v]==false && rGrafo[u][v] > 0) 
+                { 
+                    fila.add(v); 
+                    pai[v] = u; 
+                    visitado[v] = true; 
+                } 
+            } 
+        }
+       //se chegar até o terminal pela BFS saindo da fonte então returna verdadeiro, senão retorna falso
+        return (visitado[t] == true); 
+    }  
+     //retorna o valor máximo do fluxo da fonte s até o terminal t dado um grafo qualquer
+    int fordFulkerson(int grafo[][], int s, int t) 
+    { 
+        int u, v; 
+         //dado as capacidades do grafo original  
+         //elas se tornam as capacidades do grafo residual
+         // o grafo residual é rGrafo que indica a capacidade residual da aresta i até j
+         //se tiver aresta né, se rGrafo é 0, então não existe aresta de i até j 
+        for (u = 0; u < V; u++)
+        {
+            for (v = 0; v < V; v++)
+            {
+                rGrafo[u][v] = grafo[u][v];
+            }
+        }
+        // Esse vetor foi preenchido pela BFS para armazenar o caminho 
+        int pai[] = new int[V]; 
+        int max_fluxo = 0;  // Não há fluxo inicialmente 
+         //Achar um fluxo enquanto tiver um caminho da fonte até o terminal
+        while (BFS(rGrafo, s, t, pai)) 
+        {
+            //encontrar mminimo entre as arestas do grafo residual
+            //enquanto o caminho é preenchido pela BFS, achar o fluxo máximo do caminho encontrado 
+            int caminho_fluxo = Integer.MAX_VALUE; 
+            for (v=t; v!=s; v=pai[v]) 
+            { 
+                u = pai[v]; 
+                caminho_fluxo = Math.min(caminho_fluxo, rGrafo[u][v]); 
+            } 
+            //atualiza a capacidade residual das arestas e das "arestas reversas" ao longo do caminho  
+            for (v=t; v != s; v=pai[v]) 
+            { 
+                u = pai[v]; 
+                rGrafo[u][v] -= caminho_fluxo; 
+                rGrafo[v][u] += caminho_fluxo; 
+            } 
+            //Adiciona o fluxo ao fluxo total
+            max_fluxo += caminho_fluxo; 
+        } 
+        
+        /*for(int y=0;y<pai.length;y++)
+        {
+            System.out.println(pai[y]);
+        }
+        for (int[] grafo1 : grafo) {
+            System.out.println(Arrays.toString(grafo1));
+        }*/
+        //returna o fluxo máximo total
+        return max_fluxo;
+        
+    }
+    public void imprimir()
+    {
+        for(int[] rGrafo1 : rGrafo) 
+        {
+            System.out.println(Arrays.toString(rGrafo1));
+        }
+    }
+}
+    
---
+++ src/projetoes/ProjetoES.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetoes;
+
+import java.util.LinkedList;
+
+import java.util.*; 
+
+import java.lang.*; 
+
+import java.io.*;
+
+
+/**
+ *
+ * @author gabri
+ */
+public class ProjetoES 
+{
+    public static void main(String[] args) 
+    { 
+        
+        //AQUI VAMOS SUPOR QUE O OBJETIVO DO USUÁRIO SEJA DESENVOLVER UMA LINHA FERROVIÁRIA QUE AINDA NAO FOI CONSTRUIDA
+        //DESSA FORMA, ELE INSERE QUAIS AS CIDADES QUE ESTÃO LIGADAS ENTRE SI E O PROGRAMA RODA UMA SIMULAÇÃO PARA MOSTRAR
+        //QUAL SERIA O FLUXO MÁXIMO SAINDO DE UMA CIDADE DE ORIGEM E CHEGANDO EM UM DESTINO PARA ASSIM ELE INSTALAR 
+        //A MELHOR MALHA FERROVIÁRIA POSSÍVEL
+        //ELE TAMBÉM PODERÁ ESCOLHER MANUALMENTE AS CIDADES NATAIS E DESTINOS QUE QUISER A FIM DE ACHAR O MAIOR FLUXO
+        //E POR FIM ELE PODE INSERIR UMA MALHA JÁ PRONTA E PERGUNTAR QUAL SERIA A MELHOR CONFIGURAÇÃO PARA SE OBTER O FLUXO MAXIMO
+        //SUBSTUINDO APENAS A CIDADE DE ORIGEM E MANTENDO O DESTINO OU ALTERANDO O DESTINO E MANTENDO A MESMA CIDADE DE ORIGEM
+        //SIMULAÇÃO DA MELHOR ROTA PARA UMA LINHA DE ONIBUS ONDE É CONSIDERADO A MELHOR LINHA AS RUAS QUE TEM MAIOR FLUXO DE PESSOAS
+        
+        //IMPLEMENTAR UM PROGRAMA QUE GERA VALORES ALEATÓRIOS PARA AS ARESTAS QUE CONECTAM OS VERTICES DO GRAFO
+        //E COLOQUE EM UM TXT QUE VAI SER LIDO POR ESSE PROGRAMA AQUI
+        //OS VERTICES TEM QUE TER NOME DE CIDADE (NO CASO DA SIMULAÇÃO DA MALHA FERROVIARIA)
+        //OS VERTICES PARA A LINHA DE ONIBUS PODE SER PONTO1, PONTO2, PONTO3,..., PONTO_N (ONDE N É O ULTIMO PONTO)
+        //DEIXAR O SOFTWARE COMO UM EXECUTAVEL
+        /* FAZER UMA INTERFACE GRAFICA QUE VAI OFERECER AS SEGUINTES OPÇOES:
+            INTERFACE GRÁFICA PARA A MALHA FERROVIARIA:
+            1)Apenas simular, ou seja, se o usuario escolher essa opção ele vai receber apenas o fluxo máximo saindo de uma cidade e chegando a um destino     
+            2)Escolhe uma cidade de origem e uma cidade de destino e recebe o fluxo maximo entre elas (o programa tem que mostrar outras sugestões de fluxo máximo)
+            3)Escolhe uma cidade de origem e o programa escolhe o melhor o melhor destino ou escolhe o destino e deixa o programa escolher a melhor origem
+           
+            NÃO SEI O QUE FAZER PARA FAZER UMA BOA IMPLEMENTAÇÃO PARA A LINHA DE ONIBUS, ENTÃO FAÇA APENAS PARA A MALHA FERROVIARIA E DEPOIS VEMOS O QUE FAZER
+           
+            
+        */
+        
+        
+        
+        
+        
+        // Grafo exemplo
+        int Grafo[][] =new int[][] { {0, 16, 13, 0, 0, 0},{0, 0, 10, 12, 0, 0}, {0, 4, 0, 0, 14, 0},{0, 0, 9, 0, 0, 20}, {0, 0, 0, 7, 0, 4}, {0, 0, 0, 0, 0, 0}  }; 
+        MaxFlow m = new MaxFlow(); 
+        int fluxos_max[]=new int[6];
+        //System.out.println("Digite o nome do arquivo a ser lido:     ");
+        //leia o grafo do .txt aqui
+        System.out.println("Digite o vertice de saida:     ");
+        Scanner sc= new Scanner(System.in);
+        int vertice_saida=sc.nextInt();
+        System.out.println("Digite o vertice de chegada:     ");
+        int vertice_chegada=sc.nextInt();
+        int fluxo_max=m.fordFulkerson(Grafo, vertice_saida, vertice_chegada);
+        System.out.println("\nO fluxo máximo possível é: "+fluxo_max+" "+"Saindo de: "+vertice_saida+" "+"Chegando em: "+vertice_chegada); 
+        //m.imprimir();
+        int indice=0;
+        for(int x=0;x<6;x++)
+        {
+            if(x!=vertice_chegada&&x!=vertice_saida)
+            {
+                int flux= m.fordFulkerson(Grafo, vertice_saida, x);
+                System.out.println("\nOutro possivel fluxo é: "+flux+" "+"Saindo de: "+vertice_saida+" "+"Chegando em: "+x); 
+                fluxos_max[indice]=flux;
+                indice++;
+            }
+            
+        }
+        for(int y=0;y<fluxos_max.length;y++)
+        {
+            if(fluxos_max[y]>fluxo_max)
+            {
+                System.out.println("Fluxo recomendado é: "+fluxos_max[y]);
+            }
+        }
+    }
+    
+}
